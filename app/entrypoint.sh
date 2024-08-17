82fba6f4e22b5edca135db22e85cb469577269a4
pychik <gendalf.grey2014@yandex.ru>
2024-08-17 22:04:04 +0300

    Corrections: - entrypoint edited added worker recycling

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -5,7 +5,7 @@ flask db migrate
 flask db upgrade
 flask create_superuser
 
-gunicorn --workers 8 --max-requests 1000 --max-requests-jitter 50 --timeout 1000 --bind 0.0.0.0:5005 wsgi:app &
+gunicorn --workers 8 --max-requests 1000 --max-requests-jitter 50 --timeout 300 --bind 0.0.0.0:5005 wsgi:app &
 
 python3 redis_queue/worker.py &
 python3 redis_queue/scheduler.py
