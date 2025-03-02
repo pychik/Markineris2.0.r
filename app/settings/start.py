0aecb642936772e15f8b393b74c2114b15a14bde
pychik <gendalf.grey2014@yandex.ru>
2025-03-03 00:59:56 +0300

    Update: - crm даты отображения обновлены

--- app/settings/start.py
+++ app/settings/start.py
@@ -8,13 +8,14 @@ from settings.initialization import create_app
 from settings.blueprints import register_blueprints
 from settings.handlers import register_handlers, setup_login
 from .commands import create_superuser, create_superuser_custom, set_server_default_params
-from .jinja_filters import count_quantity, time_since
+from .jinja_filters import count_quantity, time_since, time_since_top
 
 urllib3.disable_warnings()
 
 # jinja template filter adding
 FILTERS["count_quantity"] = count_quantity
 FILTERS["time_since"] = time_since
+FILTERS["time_since_top"] = time_since_top
 
 migrate_handler = Migrate()
 SIMPLE_CAPTCHA = CAPTCHA(config=settings.CAPTCHA_CONFIG)
