9cd102df7742323d3f276d57cc82c8dcc826f292
Magomed Shakhbanov <shakhbanov@alfabit.org>
2025-04-08 22:07:22 +0500

    feat: Реализован ETL класс для миграции пользователя

--- app/data_migrations/etl_service.py
+++ app/data_migrations/etl_service.py
@@ -1,14 +1,44 @@
 import datetime
+import json
 from copy import deepcopy
+from io import BytesIO
+from typing import Any, TypeVar, Optional
 
 import requests
+from sqlalchemy import text
 from sqlalchemy.orm import Session
 
 from config import settings
 from data_migrations.schemas import PartnerCodeSchema, UserSchema, SchemaType
 from data_migrations.utils import get_hashed_password, make_email, make_login_name, make_password
 from logger import logger
-from models import User, PartnerCode, ModelType
+from models import (
+    User,
+    PartnerCode,
+    ModelType,
+    Price,
+    Promo,
+    Telegram,
+    TelegramMessage,
+    UserTransaction,
+    Order,
+    OrderFile,
+    OrderStat,
+    Shoe,
+    Linen,
+    Parfum,
+    Clothes,
+    Socks,
+    ShoeQuantitySize,
+    LinenQuantitySize,
+    ClothesQuantitySize,
+    SocksQuantitySize,
+)
+from redis_queue.redis_instance import get_redis_client
+from utilities.minio_service.services import get_s3_service
+from views.crm.helpers import helper_create_filename
+
+T = TypeVar("T")
 
 
 class ETLUploadProcessUserAndPartnerCode:
@@ -208,3 +238,323 @@ class ETLUploadProcessUserAndPartnerCode:
             })
 
         return data
+
+
+class ETLMigrateUserData:
+    ENDPOINTS = (
+        GET_ARCHIVE_FILE := f"{settings.MARKINERIS_API_HOST}/get_order_file_by_name",
+        GET_USER_DATA := f"{settings.MARKINERIS_API_HOST}/get_user_all_data",
+    )
+    HEADERS = {"TOKEN": settings.MARKINERS_V2_TOKEN}
+    CACHE_KEY_PATTERNS = ["order_idn_mapper*", "transaction_mapper*"]
+
+    def __init__(self, session: Session):
+        self.session = session
+        self.minio_service = get_s3_service()
+        self.redis_client = get_redis_client()
+
+    def start(self, email: str) -> dict:
+        """Запускает процесс миграции данных пользователя."""
+        response = requests.post(f"{self.GET_USER_DATA}", json={"email": email}, headers=self.HEADERS)
+        response.raise_for_status()
+
+        try:
+            self.migrate_user(response.json())
+            self.session.commit()
+            self._clear_cache()
+            return {"status": "OK"}
+        except Exception as e:
+            self.session.rollback()
+            logger.error(f"Ошибка миграции пользователя {email}: {str(e)}")
+            raise
+
+    def migrate_user(self, user_data: dict[str, Any], admin_parent_id: Optional[int] = None) -> None:
+        """Миграция данных пользователя."""
+        defaults = {
+            "admin_parent_id": admin_parent_id,
+            "price_id": None,
+            "admin_order_num": 0,
+        }
+
+        user_data = self._clear_data_before_save(user_data, defaults)
+
+        nested_data = {
+            "transactions": user_data.pop("transactions", []),
+            "orders": user_data.pop("orders", []),
+            "orders_stats": user_data.pop("orders_stats", []),
+            "telegram": user_data.pop("telegram", []),
+            "partners": user_data.pop("partners", []),
+            "promos": user_data.pop("promos", []),
+            "telegram_message": user_data.pop("telegram_message", []),
+            "prices": user_data.pop("prices", {}),
+            "admin_group": user_data.pop("admin_group", []),
+        }
+
+        user = self._create(
+            User,
+            user_data,
+            defaults,
+        )
+
+        if nested_data["prices"]:
+            price_data = nested_data["prices"]
+            if "id" in price_data:
+                del price_data["id"]
+            price = self._get_or_create(Price, price_data, filters={"price_code": price_data["price_code"]})
+            user.price_id = price.id
+            self.session.flush([user])
+
+        # Обработка вложенных списков
+        self._process_nested_list(nested_data["partners"], PartnerCode, user, "partners")
+        self._process_nested_list(nested_data["promos"], Promo, user, "promos")
+        self._process_nested_list(
+            nested_data["telegram"],
+            Telegram,
+            user,
+            "telegram",
+            {"user_id": user.id},
+        )
+        self._process_nested_list(
+            nested_data["telegram_message"],
+            TelegramMessage,
+            user,
+            "telegram_message",
+            {"user_id": user.id},
+        )
+
+        # Обработка транзакций
+        for transaction in nested_data["transactions"]:
+            old_id = transaction.pop("id", None)
+            transaction["user_id"] = user.id
+            new_transaction = self._create(UserTransaction, transaction)
+            if old_id:
+                self.redis_client.set(f"transaction_mapper:{old_id}", json.dumps(new_transaction.id))
+
+        # Обработка заказов
+        for order in nested_data["orders"]:
+            self._migrate_order(order, user)
+
+        # Обработка статистики заказов
+        for order_stat in nested_data["orders_stats"]:
+            self._migrate_order_stat(order_stat, user)
+
+        # Рекурсивная миграция admin_group
+        for admin_user in nested_data["admin_group"]:
+            self.migrate_user(admin_user, admin_parent_id=user.id)
+
+    def create_order_category(self, item: dict, order: Order) -> None:
+        """Создание категории заказа."""
+        category_model_mapper = {
+            "обувь": Shoe,
+            "белье": Linen,
+            "парфюм": Parfum,
+            "одежда": Clothes,
+            "носки и прочее": Socks,
+        }
+        size_quantity_model_mapper = {
+            "обувь": ShoeQuantitySize,
+            "белье": LinenQuantitySize,
+            "одежда": ClothesQuantitySize,
+            "носки и прочее": SocksQuantitySize,
+        }
+        category_field_mapper = {
+            "обувь": "shoe_id",
+            "белье": "lin_id",
+            "одежда": "cl_id",
+            "носки и прочее": "socks_id",
+        }
+
+        sizes_quantities = item.pop("sizes_quantities", [])
+        if "id" in item:
+            del item["id"]
+        item["order_id"] = order.id
+        category = self._create(category_model_mapper[order.category], item)
+
+        for size_quantity in sizes_quantities:
+            if "id" in size_quantity:
+                del size_quantity["id"]
+            size_quantity[category_field_mapper[order.category]] = category.id
+            self._create(size_quantity_model_mapper[order.category], size_quantity)
+
+    def get_order_idn(self, user: User) -> str:
+        """Генерация нового order_idn."""
+        admin_id = user.admin_parent_id if user.role == settings.ORD_USER else user.id
+        stmt = text(
+            """UPDATE public.users SET admin_order_num=admin_order_num + 1 
+               WHERE id=:admin_id RETURNING admin_order_num, is_crm, is_at2;"""
+        ).bindparams(admin_id=admin_id)
+        res = self.session.execute(stmt).fetchone()
+        self.session.flush()
+        return f"{admin_id}_{res.admin_order_num}"
+
+    def _get_or_create(
+            self,
+            model: type[T],
+            data: dict[str, Any],
+            filters: dict[str, Any],
+    ) -> T:
+        """Универсальный метод для создания или получения объекта."""
+        instance = self.session.query(model).filter_by(**filters).first()
+        if not instance:
+            if "id" in data:
+                del data["id"]
+            instance = model(**data)
+            self.session.add(instance)
+            self.session.flush([instance])
+
+        return instance
+
+    def _create(
+            self,
+            model: type[T],
+            data: dict[str, Any],
+            defaults: dict[str, Any] | None = None,
+    ) -> T:
+        """Универсальный метод для создания объекта."""
+        if "id" in data:
+            del data["id"]
+        try:
+            instance = model(**data)
+            self.session.add(instance)
+            self.session.flush([instance])
+            if defaults:
+                for key, value in defaults.items():
+                    setattr(instance, key, value)
+            return instance
+        except Exception as e:
+            print(e)
+
+    def _process_nested_list(
+            self,
+            items: list[dict],
+            model: type[T],
+            parent: T,
+            relation_field: str,
+            extra_data: dict = None,
+    ) -> None:
+        """Обрабатывает список вложенных объектов и связывает их с родителем."""
+        for item in items or []:
+            if "id" in item:
+                del item["id"]
+            defaults = extra_data or {}
+            item = self._clear_data_before_save(item, defaults)
+            if model in [PartnerCode, Promo, Telegram]:
+                filters = {"code": item["code"]} if "code" in item else {}
+                filters.update({"name": item["name"]} if "name" in item else {})
+                item.pop("type", None)
+                instance = self._get_or_create(model, item, filters)
+            else:
+                instance = self._create(model, item, defaults)
+            getattr(parent, relation_field).append(instance)
+            self.session.flush([instance, parent])
+
+    def _get_manager_id(self, manager_name: Optional[str]) -> Optional[int]:
+        """Получает ID менеджера по его имени."""
+        if manager_name:
+            manager = self.session.query(User).filter_by(login_name=manager_name).first()
+            return manager.id if manager else None
+        return None
+
+    def _get_transaction_id(self, old_transaction_id: Optional[int]) -> Optional[int]:
+        """Получает актуальный ID транзакции из кеша."""
+        if old_transaction_id:
+            cached_id = self.redis_client.get(f"transaction_mapper:{old_transaction_id}")
+            return json.loads(cached_id) if cached_id else None
+        return None
+
+    def _get_order_idn(self, order_data: dict, user: User) -> str | None:
+        """Генерирует или получает order_idn для заказа."""
+        if not order_data.get("order_idn"):
+            return None
+        key = f"order_idn_mapper:{order_data['order_idn']}"
+        new_idn = self.get_order_idn(user)
+        self.redis_client.set(key, json.dumps(new_idn))
+        return new_idn
+
+    def _migrate_order_file(self, file_data: dict, order: Order, manager_id: Optional[int]) -> None:
+        """Миграция файла заказа."""
+        if "id" in file_data:
+            del file_data["id"]
+        manager_name = self.session.query(User).get(manager_id).login_name if manager_id else ""
+        origin_name, fs_name = helper_create_filename(
+            order_idn=order.order_idn or "",
+            manager_name=manager_name,
+            filename=file_data["file_system_name"]
+        )
+
+        file_response = requests.get(
+            f"{self.GET_ARCHIVE_FILE}/{file_data['file_system_name']}",
+            headers=self.HEADERS,
+        )
+        if file_response.ok:
+            file_data_stream = BytesIO(file_response.content)
+            self.minio_service.upload_file(
+                file_data=file_data_stream,
+                object_name=fs_name,
+                bucket_name=settings.MINIO_CRM_BUCKET_NAME
+            )
+
+        file_data["file_system_name"] = fs_name
+        file_data["origin_name"] = origin_name
+        file_data["order_id"] = order.id
+        self._create(OrderFile, file_data)
+
+    def _migrate_order(self, order_data: dict, user: User) -> None:
+        """Миграция данных заказа."""
+        category_mapper = {
+            settings.Shoes.CATEGORY: {"field_name": "shoes", "model": Shoe},
+            settings.Linen.CATEGORY: {"field_name": "linen", "model": Linen},
+            settings.Parfum.CATEGORY: {"field_name": "parfum", "model": Parfum},
+            settings.Clothes.CATEGORY: {"field_name": "clothes", "model": Clothes},
+            settings.Socks.CATEGORY: {"field_name": "socks", "model": Socks},
+        }
+
+        categories = order_data.pop(category_mapper[order_data["category"]]["field_name"], [])
+        order_zip_file = order_data.pop("order_zip_file", {})
+
+        order = self._common_order_create_method(data=order_data, user=user, model=Order)
+
+        if order_zip_file and "id" in order_zip_file:
+            self._migrate_order_file(order_zip_file, order, order.manager_id)
+
+        for category in categories:
+            self.create_order_category(category, order)
+
+    def _migrate_order_stat(self, order_stat_data: dict, user: User) -> None:
+        """Миграция статистики заказа."""
+        self._common_order_create_method(order_stat_data, user, OrderStat)
+
+    def _common_order_create_method(self, data: dict[str, Any], user: User, model: type[T]) -> T:
+        manager_id = self._get_manager_id(data.pop("manager_id", None))
+        transaction_id = self._get_transaction_id(data.pop("transaction_id", None))
+        order_idn = self._get_order_idn(data, user)
+        if "id" in data:
+            del data["id"]
+
+        defaults = {
+            "user_id": user.id,
+            "manager_id": manager_id,
+            "transaction_id": transaction_id,
+            "order_idn": order_idn
+        }
+        data = self._clear_data_before_save(data, defaults)
+
+        return self._create(model, data, defaults)
+
+    @staticmethod
+    def _clear_data_before_save(data: dict[str, Any], defaults: dict[str, Any]) -> dict[str, Any]:
+        for key, value in defaults.items():
+            data.pop(key, None)
+
+        return data
+
+    def _clear_cache(self) -> None:
+        """Очищает кеш от неактуальных ключей."""
+        try:
+            to_unlink_keys = [
+                key for pattern in self.CACHE_KEY_PATTERNS for key in self.redis_client.keys(pattern)
+            ]
+            if to_unlink_keys:
+                self.redis_client.unlink(*to_unlink_keys)
+        except Exception as e:
+            logger.error(f"Не удалось очистить кеш: {str(e)}")
